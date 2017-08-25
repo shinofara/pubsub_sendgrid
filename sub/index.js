90e5ce0961eb6f670d8cb004a434ab5d40c56812
shinofara <shinofara@gmail.com>
2017-08-25 15:32:28 +0900

    メールを受け取ったパラメータ毎に

--- sub/index.js
+++ sub/index.js
@@ -14,18 +14,22 @@ exports.helloPubSub = function (event, callback) {
     var sendgrid   = require('sendgrid')(api_key);
     var email      = new sendgrid.Email();
 
+    tmplFilePath = "template/" + data.template + "/" + data.type + ".ejs"
+    var fs = require('fs');
+    var templateString = fs.readFileSync(tmplFilePath, 'utf-8');
+    
     console.log(data)
     var ejs = require('ejs'),
-        html = ejs.render('<html><body><%= people %></body></html>', {people: data.name});
+        html = ejs.render(templateString, data.data)
 
-    email.setTos(tos);
-    email.setFrom(from);
-    email.fromname = '送信者名';
-    email.setSubject('Hello Pub/Sub');
-    email.setText('text');
+    email.setTos(data.to);
+    email.setFrom(data.from);
+    email.fromname = data.fromName;
+    email.setSubject(data.subject);
+    //email.setText('text');
     email.setHtml(html);
     email.addHeader('X-Sent-Using', 'SendGrid-API');
-    email.addFile({path: './gif.gif', filename: 'owl.gif'});
+    //email.addFile({path: './gif.gif', filename: 'owl.gif'});
 
     sendgrid.send(email, function(err, json) {
         if (err) { return console.error(err); }
