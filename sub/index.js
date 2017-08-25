99bff4f35c2eeb4310f9d1fe2f873f7879bb0532
shinofara <shinofara@gmail.com>
2017-08-25 15:57:38 +0900

    text,htmlの出し分け

--- sub/index.js
+++ sub/index.js
@@ -13,22 +13,30 @@ exports.helloPubSub = function (event, callback) {
 
     var sendgrid   = require('sendgrid')(api_key);
     var email      = new sendgrid.Email();
-
-    tmplFilePath = "template/" + data.template + "/" + data.type + ".ejs"
-    var fs = require('fs');
-    var templateString = fs.readFileSync(tmplFilePath, 'utf-8');
+    var ejs = require('ejs')
     
-    console.log(data)
-    var ejs = require('ejs'),
-        html = ejs.render(templateString, data.data)
-
     email.setTos(data.to);
     email.setFrom(data.from);
     email.fromname = data.fromName;
     email.setSubject(data.subject);
-    //email.setText('text');
-    email.setHtml(html);
     email.addHeader('X-Sent-Using', 'SendGrid-API');
+
+    var fs = require('fs');
+
+    if (data.type == "html" || data.type == "all") {
+        tmplFilePath = "template/" + data.template + "/html.ejs"
+        htmlTemplate = fs.readFileSync(tmplFilePath, 'utf-8');
+        var html = ejs.render(htmlTemplate, data.data)
+        email.setHtml(html);
+    }
+
+    if (data.type == "text" || data.type == "all") {
+        tmplFilePath = "template/" + data.template + "/text.ejs"
+        textTemplate = fs.readFileSync(tmplFilePath, 'utf-8');
+
+        var text = ejs.render(textTemplate, data.data) 
+        email.setText(text);
+    }
     //email.addFile({path: './gif.gif', filename: 'owl.gif'});
 
     sendgrid.send(email, function(err, json) {
