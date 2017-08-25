0fb1481a39bb857f95da5c3d85963a26305b67e6
shinofara <shinofara@gmail.com>
2017-08-25 12:22:25 +0900

    pub/subからのメッセージをオブジェクトに分解

--- sub/index.js
+++ sub/index.js
@@ -1,7 +1,7 @@
 exports.helloPubSub = function (event, callback) {
     const pubsubMessage = event.data;
-    const name = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : 'World';
-
+    const str = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : 'World';
+    data = JSON.parse(str)
 
     var dotenv = require('dotenv');
     dotenv.load();
@@ -14,8 +14,9 @@ exports.helloPubSub = function (event, callback) {
     var sendgrid   = require('sendgrid')(api_key);
     var email      = new sendgrid.Email();
 
+    console.log(data)
     var ejs = require('ejs'),
-    html = ejs.render('<html><body><%= people %></body></html>', {people: name});
+        html = ejs.render('<html><body><%= people %></body></html>', {people: data.name});
 
     email.setTos(tos);
     email.setFrom(from);
@@ -26,9 +27,6 @@ exports.helloPubSub = function (event, callback) {
     email.addHeader('X-Sent-Using', 'SendGrid-API');
     email.addFile({path: './gif.gif', filename: 'owl.gif'});
 
-
-
-    console.log(`Hello, ${name}!`);
     sendgrid.send(email, function(err, json) {
         if (err) { return console.error(err); }
         console.log(json);
